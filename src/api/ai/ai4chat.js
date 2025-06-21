98f3f0381e7eda141c8ecdb7f804e0dd00798b33
alfinofc1 <160604519+alfinofc1@users.noreply.github.com>
2025-06-21 19:03:23 +0700

    Add files via upload

--- src/api/ai/ai4chat.js
+++ src/api/ai/ai4chat.js
@@ -3,28 +3,28 @@ const axios = require('axios');
 module.exports = function(app) {
     async function fetchAi4chat(text) {
         try {
-            const url = `https://api.nekorinn.my.id/ai-img/ai4chat?text=${encodeURIComponent(text)}&ratio=${encodeURIComponent(ratio)}`;
+            const model = encodeURIComponent(`1:1,16:9,2:3,3:2,4:5,5:4,9:16,21:9,9:21`);
+            const url = `https://api.nekorinn.my.id/ai-img/ai4chat?text=${encodeURIComponent(text)}&ratio=${encodeURIComponent(model)}`;
             const response = await axios.get(url);
             return response.data;
         } catch (error) {
-            console.error("Error fetching from Copilot AI:", error.response?.data || error.message);
-            throw new Error("Gagal terhubung ke Copilot AI.");
+            console.error("Error fetching from IraaAi:", error.response?.data || error.message);
+            throw new Error("Gagal terhubung ke Server IraaAi.");
         }
     }
 
     app.get('/ai/ai4chat', async (req, res) => {
         try {
-            const { text, model } = req.query;
-            if (!text || !model) {
-                return res.status(400).json({ status: false, error: 'Text and Model is required' });
+            const { text } = req.query;
+            if (!text) {
+                return res.status(400).json({ status: false, error: 'Text is required' });
             }
-
             const result = await fetchAi4chat(text);
 
             res.status(200).json({
                 status: true,
                 creator: "Hazel",
-                result: result.result || result.message || "Tidak ada respons dari Copilot AI ðŸ¥º"
+                result: result.result || result.message || "Tidak ada respons dari IraaAi ðŸ¥º"
             });
         } catch (error) {
             res.status(500).json({
