c404265e0601edf69a008dab59412231b3ebb17d
alfinofc1 <160604519+alfinofc1@users.noreply.github.com>
2025-06-21 19:13:40 +0700

    Add files via upload

--- src/api/ai/ai4chat.js
+++ src/api/ai/ai4chat.js
@@ -3,13 +3,12 @@ const axios = require('axios');
 module.exports = function(app) {
     async function fetchAi4chat(text) {
         try {
-            const model = encodeURIComponent(`hai);
-            const url = `https://api.nekorinn.my.id/ai-img/ai4chat?text=${encodeURIComponent(text)}&ratio=${encodeURIComponent(model)}`;
+            const url = `https://api.nekorinn.my.id/ai-img/ai4chat?text=${encodeURIComponent(text)}&ratio=${encodeURIComponent(model)`;
             const response = await axios.get(url);
             return response.data;
         } catch (error) {
-            console.error("Error fetching from IraaAi:", error.response?.data || error.message);
-            throw new Error("Gagal terhubung ke Server IraaAi.");
+            console.error("Error fetching from llama-3.3-70b:", error.response?.data || error.message);
+            throw new Error("Gagal terhubung ke llama-3.3-70b");
         }
     }
 
@@ -18,18 +17,22 @@ module.exports = function(app) {
             const { text } = req.query;
             if (!text) {
                 return res.status(400).json({ status: false, error: 'Text is required' });
+                const { model } = req.query;
+            if (!model) {
+                return res.status(400).json({ status: false, error: 'model is required' });
             }
+
             const result = await fetchAi4chat(text);
 
             res.status(200).json({
                 status: true,
-                creator: "Hazel",
-                result: result.result || result.message || "Tidak ada respons dari IraaAi ðŸ¥º"
+                creator: "alfin",
+                result: result.result || result.message || "Tidak ada respons dari llama-3.3-70b AI ðŸ¥º"
             });
         } catch (error) {
             res.status(500).json({
                 status: false,
-                creator: "Hazel",
+                creator: "alfin",
                 result: "Maaf, terjadi kesalahan saat memproses permintaan Anda."
             });
         }
